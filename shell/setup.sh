85f4f17c57a5382c6f41a44583ea8b7713fda197
on-jungwoan <slalfpdl16@naver.com>
2025-03-27 16:44:31 +0900

    나머지 파일 수정

--- shell/setup.sh
+++ shell/setup.sh
@@ -5,14 +5,11 @@ conda create -n mask python==3.10
 conda activate mask
 
 # install torch
-conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.7 -c pytorch -c nvidia
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
 # install requirements
+pip install -e segment_anything_2
 pip install -r requirements.txt
-
-# download datasets
-gdown 1xuSMH7ZkfQLvb7KGJJuhP90LCd1uqjXT
-tar -zxvf dataset.tar.gz && rm dataset.tar.gz
 
 # download ckpts
 cd checkpoints
