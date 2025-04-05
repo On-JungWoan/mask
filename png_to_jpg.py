0a01b064f26d33c95cb28e2182867b0b57219570
on-jungwoan <slalfpdl16@naver.com>
2025-04-05 22:18:28 +0900

    some add

---
+++ png_to_jpg.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+import argparse
+import os.path as op
+from PIL import Image
+
+def main(args):
+    root_dir = args.root_dir
+    backup_dir = op.join(root_dir, '../backup')
+    seq_name = root_dir.split('/')[-1]
+    
+    #* bu
+    shutil.move(root_dir, backup_dir)
+    
+    #* mkdir
+    os.makedirs(backup_dir, exist_ok=True)
+    os.makedirs(root_dir, exist_ok=True)
+    
+    tgt_dir = op.join(backup_dir, seq_name)
+    for img_name in sorted(os.listdir(tgt_dir)):
+        img = Image.open(op.join(tgt_dir, img_name))
+        img.save(op.join(root_dir, img_name).replace('png', 'jpg'))
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--root_dir", type=str, default='/home/user/ojw/BIGS_release/ml-hugs/preprocess/mask/inputs/custom')
+    
+    args = parser.parse_args()
+    
+    main(args)
